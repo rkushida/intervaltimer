8db5e2243d9a31009e4c8290531b4085df2b5477
rkushida <137481609+rkushida@users.noreply.github.com>
2024-03-15 22:10:15 +0900

    --amend

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,5 @@
-export function formatMinSec(minutes: number, seconds: number) {
-	const min = String(minutes).padStart(2, "0");
-	const sec = String(seconds).padStart(2, "0");
-	return `${min}:${sec}`;
+export function divmod(x: number, y: number): [number, number] {
+	return [Math.floor(x / y), x % y];
 }
 
 export function sec2MinSec(time: number) {
@@ -12,10 +10,12 @@ export function minSec2Sec(minutes: number, seconds: number) {
 	return minutes * 60 + seconds;
 }
 
-export function divmod(x: number, y: number): [number, number] {
-	return [Math.floor(x / y), x % y];
-}
-
 export function ms2Sec(time: number) {
 	return Math.ceil(time / 1000);
+}
+
+export function formatMinSec(minutes: number, seconds: number) {
+	const min = String(minutes).padStart(2, "0");
+	const sec = String(seconds).padStart(2, "0");
+	return `${min}:${sec}`;
 }
